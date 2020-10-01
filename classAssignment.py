6bed50401f37940c7ee1e13d28b557e424d8d470
r3bunker <r3bunker@gmail.com>
2020-10-01 13:50:51 -0600

    create class with children

---
+++ classAssignment.py
@@ -0,0 +1,20 @@
+
+class Nature:
+    species = ""
+    location = "Africa"
+    nutrition = ""
+
+
+class Animal(Nature):
+    predator = "true"
+    movement = "quadruped"
+
+class Plant(Nature):
+    rootSystem = ""
+    defense = "thorns"
+
+    
+
+    
+    
+    

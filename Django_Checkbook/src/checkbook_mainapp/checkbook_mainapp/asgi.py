1bcab26fe30be2c995ca3690ee3570c840847153
r3bunker <r3bunker@gmail.com>
2020-10-20 16:04:41 -0600

    Added checkbook assignment

---
+++ Django_Checkbook/src/checkbook_mainapp/checkbook_mainapp/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for checkbook_mainapp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'checkbook_mainapp.settings')
+
+application = get_asgi_application()
